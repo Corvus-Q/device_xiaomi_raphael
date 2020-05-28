95dd092e66fd21510e433b9db8a75bed7d26327f
riteshm321 <riteshm321@gmail.com>
2020-05-28 18:00:50 +0530

    treewide: corvus bringup

--- du_raphael.mk
+++ du_raphael.mk
@@ -7,14 +7,18 @@
 $(call inherit-product, device/xiaomi/raphael/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/du/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := raphael
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := lineage_raphael
+PRODUCT_NAME := du_raphael
 
 BUILD_FINGERPRINT := "Xiaomi/raphaelin/raphaelin:9/PKQ1.181121.001/V10.3.3.0.PFKINXM:user/release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Official
+DU_BUILD_TYPE := OFFICIAL
+EXTRA_FOD_ANIMATIONS := true
